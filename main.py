cda4d504400e871f4da40af4cab045531e2c0abd
matsuo-develop-tada <matsuo.develop+tada@gmail.com>
2021-03-30 23:12:04 +0900

    Todo一覧を返すように

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('環境構築は成功だよ！')
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+todos = [
+    {'id': 1, 'content': 'todo1'},
+    {'id': 2, 'content': 'todo2'},
+    {'id': 3, 'content': 'todo3'},
+]
+
+@app.route('/todos')
+def get_todos():
+    return jsonify(todos)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
