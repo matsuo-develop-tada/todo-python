aa576184650e67f8c26b25d053d1ef26c7b32375
masasami <masangular0518@gmail.com>
2021-04-11 21:00:07 +0900

    Todoの項目を追加

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 from os.path import join, dirname
 from dotenv import load_dotenv
+from datetime import datetime
 
 # 環境変数をどのファイルから読み込むかを設定
 load_dotenv(verbose=True)
@@ -28,15 +29,29 @@ app.config['SECRET_KEY'] = secret_key
 app.config['SQLALCHEMY_DATABASE_URI'] = uri
 db = SQLAlchemy(app)
 
+
 # Todoモデル
 class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'todo'
+
+    id_todo = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(500))
+    color_code = db.Column(db.String(10))
+    checked = db.Column(db.Boolean)
+    dt_do = db.Column(db.DateTime)
+    dt_create = db.Column(db.DateTime, default=datetime.utcnow)
+    dt_update = db.Column(db.DateTime, onupdate=datetime.utcnow)
 
-    def __init__(self, content):
+    def __init__(self, content, color_code, checked, dt_do):
         self.content = content
+        self.color_code = color_code
+        self.checked = checked
+        self.dt_do = dt_do
 
+
+# db.drop_all()
 db.create_all()
+
 
 @app.route('/todos', methods=['GET'])
 def get_todos():
@@ -46,11 +61,25 @@ def get_todos():
     # Todo一覧を取得した後に、dict→jsonの順に変換して返す
     dict_todos = []
     for todo in todos:
-        dict_todo = {}
-        dict_todo['id'] = todo.id
-        dict_todo['content'] = todo.content
-        dict_todos.append(dict_todo)
+        dict_todos.append({
+            'id_todo': todo.id_todo,
+            'content': todo.content,
+            'color_code': todo.color_code,
+            'checked': todo.checked,
+            'dt_do': format_date(todo.dt_do),
+            'dt_create': format_date(todo.dt_create),
+            'dt_update': format_date(todo.dt_update)
+        })
+
     return jsonify(dict_todos)
+
+
+# datetimeを「%Y-%m-%d %H:%M:%S」形式に変換するメソッド
+def format_date(datetime):
+    if datetime is None:
+        return None
+    return datetime.strftime('%Y-%m-%d %H:%M:%S')
+
 
 # Flask起動
 if __name__ == '__main__':
