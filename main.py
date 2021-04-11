02a28680cc336af6271dcd97b85668d4881cc13e
masasami <masangular0518@gmail.com>
2021-04-11 22:01:10 +0900

    ファイル分割

--- main.py
+++ main.py
@@ -1,56 +1,5 @@
-from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from os import getenv
-from os.path import join, dirname
-from dotenv import load_dotenv
-from datetime import datetime
-
-# 環境変数をどのファイルから読み込むかを設定
-load_dotenv(verbose=True)
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-
-# 環境変数の取得
-dialect = getenv('DIALECT')
-driver = getenv('DRIVER')
-db_user = getenv('DB_USER')
-db_pass = getenv('DB_PASS')
-db_host = getenv('DB_HOST')
-db_port = getenv('DB_PORT')
-db_name = getenv('DB_NAME')
-secret_key = getenv('SECRET_KEY')
-
-# DB接続用URI生成
-uri = f'{dialect}+{driver}://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
-
-# Flask
-app = Flask(__name__)
-app.config['SECRET_KEY'] = secret_key
-app.config['SQLALCHEMY_DATABASE_URI'] = uri
-db = SQLAlchemy(app)
-
-
-# Todoモデル
-class Todo(db.Model):
-    __tablename__ = 'todo'
-
-    id_todo = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(500))
-    color_code = db.Column(db.String(10))
-    checked = db.Column(db.Boolean)
-    dt_do = db.Column(db.DateTime)
-    dt_create = db.Column(db.DateTime, default=datetime.utcnow)
-    dt_update = db.Column(db.DateTime, onupdate=datetime.utcnow)
-
-    def __init__(self, content, color_code, checked, dt_do):
-        self.content = content
-        self.color_code = color_code
-        self.checked = checked
-        self.dt_do = dt_do
-
-
-# db.drop_all()
-db.create_all()
+from flask import jsonify
+from settings import app, Todo
 
 
 @app.route('/todos', methods=['GET'])
@@ -58,27 +7,12 @@ def get_todos():
     # Todo一覧を取得
     todos = Todo.query.all()
 
-    # Todo一覧を取得した後に、dict→jsonの順に変換して返す
+    # Todo一覧を取得した後に、class→dict→jsonの順に変換して返す
     dict_todos = []
     for todo in todos:
-        dict_todos.append({
-            'id_todo': todo.id_todo,
-            'content': todo.content,
-            'color_code': todo.color_code,
-            'checked': todo.checked,
-            'dt_do': format_date(todo.dt_do),
-            'dt_create': format_date(todo.dt_create),
-            'dt_update': format_date(todo.dt_update)
-        })
+        dict_todos.append(todo.to_dict())
 
     return jsonify(dict_todos)
-
-
-# datetimeを「%Y-%m-%d %H:%M:%S」形式に変換するメソッド
-def format_date(datetime):
-    if datetime is None:
-        return None
-    return datetime.strftime('%Y-%m-%d %H:%M:%S')
 
 
 # Flask起動
--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
-from sqlalchemy import create_engine
+from datetime import datetime
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 from os.path import join, dirname
 from dotenv import load_dotenv
@@ -16,23 +18,56 @@ db_pass = getenv('DB_PASS')
 db_host = getenv('DB_HOST')
 db_port = getenv('DB_PORT')
 db_name = getenv('DB_NAME')
-charset = getenv('CHARSET')
+secret_key = getenv('SECRET_KEY')
 
-# DB接続用url生成
-url = f'{dialect}+{driver}://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
-print(url)
+# DB接続用URI生成
+uri = f'{dialect}+{driver}://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
 
-# DB接続
-engine = create_engine(url, echo=True)
-print(engine)
+# Flask
+app = Flask(__name__)
+app.config['SECRET_KEY'] = secret_key
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
+db = SQLAlchemy(app)
 
-engine.execute('DROP TABLE IF EXISTS todo')
-engine.execute('CREATE TABLE todo (id INT PRIMARY KEY AUTO_INCREMENT, content VARCHAR(500))')
-engine.execute("INSERT INTO todo (content) VALUES ('todo1')")
-engine.execute("INSERT INTO todo (content) VALUES ('todo2')")
-engine.execute("INSERT INTO todo (content) VALUES ('todo3')")
-todos = engine.execute('SELECT * FROM todo')
 
-for todo in todos:
-    print(todo)
+# Todoモデル
+class Todo(db.Model):
+    __tablename__ = 'todo'
 
+    id_todo = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(500))
+    color_code = db.Column(db.String(10))
+    checked = db.Column(db.Boolean)
+    dt_do = db.Column(db.DateTime)
+    dt_create = db.Column(db.DateTime, default=datetime.utcnow)
+    dt_update = db.Column(db.DateTime, onupdate=datetime.utcnow)
+
+    # dictに変換
+    def to_dict(self):
+        return {
+            'id_todo': self.id_todo,
+            'content': self.content,
+            'color_code': self.color_code,
+            'checked': self.checked,
+            'dt_do': format_date(self.dt_do),
+            'dt_create': format_date(self.dt_create),
+            'dt_update': format_date(self.dt_update)
+        }
+
+    def __init__(self, content, color_code, checked, dt_do):
+        self.content = content
+        self.color_code = color_code
+        self.checked = checked
+        self.dt_do = dt_do
+
+
+# datetimeを「%Y-%m-%d %H:%M:%S」形式に変換するメソッド
+def format_date(datetime):
+    if datetime is None:
+        return None
+    return datetime.strftime('%Y-%m-%d %H:%M:%S')
+
+
+# 定義したモデルをDBに反映
+# db.drop_all()
+db.create_all()
